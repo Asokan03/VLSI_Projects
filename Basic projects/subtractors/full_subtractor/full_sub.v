dc816f0c5a58183fcdf3aaf71eaff9af948e959a
Asokan K <asokankarunanidhi06@gmail.com>
2025-07-14 15:32:12 +0530

    Update full_sub.v

--- Basic projects/subtractors/full_subtractor/full_sub.v
+++ Basic projects/subtractors/full_subtractor/full_sub.v
@@ -1,1 +1,14 @@
+module full_subtractor (
+    input a,
+    input b,
+    input bin,         // Borrow in
+    output diff,
+    output bout        // Borrow out
+);
+
+// Logic equations
+assign diff = a ^ b ^ bin;
+assign bout = (~a & b) | (~(a ^ b) & bin);
+
+endmodule
 
